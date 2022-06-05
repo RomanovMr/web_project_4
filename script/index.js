3b059bc30863e5ad67c21e29fd3a62667c5414ec
RomanovStas <waiff@yandex.ru>
2022-06-05 23:24:23 +0300

    BEM and buttons repair

--- script/index.js
+++ script/index.js
@@ -1,15 +1,15 @@
 const popupDisplay = document.querySelector(".popup");
-const popupOpen = document.querySelector(".profile__button_edit");
-const popupClose = document.querySelector(".popup__button_close");
-const likeButton = document.querySelector(".grid__card_button-like");
+const popupOpen = document.querySelector(".profile__button_type_edit");
+const popupClose = document.querySelector(".popup__button");
+const likeButton = document.querySelector(".grid__card-button-like");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__subtitle");
-const nameInput = document.querySelector(".popup__form_name");
-const aboutInput = document.querySelector(".popup__form_about");
+const nameInput = document.querySelector(".popup__input_type_name");
+const aboutInput = document.querySelector(".popup__input_type_about");
 const form = document.querySelector(".popup__form");
 
 function toggleModal() {
-  popupDisplay.classList.toggle("popup__opened");
+  popupDisplay.classList.toggle("popup_opened");
 }
 
 popupOpen.addEventListener("click", function () {
